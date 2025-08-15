a8f4919ddaf63913cdc90b99563c56cc9ae8b12d
gusjengis <anthony.j.green@outlook.com>
2025-08-15 11:16:04 -0700

    simplified flake

--- flake.nix
+++ flake.nix
@@ -20,17 +20,10 @@
       in
       {
         devShells = {
-          # Default shell: Wayland + X11 + Vulkan, good for both backends.
           default = pkgs.mkShell {
             packages = with pkgs; [
-              # toolchain / build
               rustup
-              cargo
-              clang
-              cmake
-              pkg-config
 
-              # runtime libs winit dlopens at runtime
               wayland
               libxkbcommon
               xorg.libX11
@@ -39,12 +32,8 @@
               xorg.libXi
               xorg.libXrandr
 
-              # Vulkan loader + Mesa ICDs (for AMD/Intel or software llvmpipe)
-              vulkan-loader
               mesa
 
-              # debug tools
-              vulkan-tools # gives `vulkaninfo`
             ];
 
             # Make sure dynamic libs are found at runtime (esp. when running via `cargo run`)
@@ -52,51 +41,10 @@
                           export LD_LIBRARY_PATH=${pkgs.wayland}/lib:${pkgs.libxkbcommon}/lib:\
               ${pkgs.xorg.libX11}/lib:${pkgs.xorg.libxcb}/lib:${pkgs.xorg.libXcursor}/lib:${pkgs.xorg.libXi}/lib:${pkgs.xorg.libXrandr}/lib:\
               ${pkgs.mesa.drivers}/lib:${pkgs.mesa.drivers}/lib/dri:${pkgs.vulkan-loader}/lib:\
-              $LD_LIBRARY_PATH
-
-                          echo
-                          echo "🥽 Quick checks:"
-                          echo "  - vulkaninfo | head"
-                          echo "  - RUST_LOG=wgpu_hal=debug,winit=info WGPU_BACKEND=vulkan cargo run"
-                          echo "  - Force X11 for a sanity check: WINIT_UNIX_BACKEND=x11 cargo run"
-                          echo
-
-                          # helpful default for debug runs; comment out if noisy:
             '';
 
-            # Let pkg-config see Wayland/X11 if anything uses it at build time
-            PKG_CONFIG_PATH = pkgs.lib.makeSearchPath "lib/pkgconfig" [
-              pkgs.wayland
-              pkgs.libxkbcommon
-              pkgs.xorg.libX11
-              pkgs.xorg.libxcb
-              pkgs.xorg.libXcursor
-              pkgs.xorg.libXi
-              pkgs.xorg.libXrandr
-            ];
-
-            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
           };
 
-          # Convenience shell that forces X11 (sometimes easier to get going first)
-          x11 = pkgs.mkShell {
-            inputsFrom = [ self.devShells.${system}.default ];
-            shellHook = ''
-              ${self.devShells.${system}.default.shellHook}
-              export WINIT_UNIX_BACKEND=x11
-              echo "🔧 Forcing X11 backend (WINIT_UNIX_BACKEND=x11)."
-            '';
-          };
-
-          # Convenience shell that forces Wayland
-          wayland = pkgs.mkShell {
-            inputsFrom = [ self.devShells.${system}.default ];
-            shellHook = ''
-              ${self.devShells.${system}.default.shellHook}
-              unset WINIT_UNIX_BACKEND
-              echo "🟢 Using Wayland backend (default)."
-            '';
-          };
         };
       }
     );
